403426a7fb47746bfda051e098cd811a853a9892
Misbah Yunus <misbah.yunus@outlook.com>
2016-04-08 16:51:33 +1000

    Added the option for user to enter his/her own base currency. If base currency is changed from last run, then data file is recreated with new values.

--- TestCurrencyManager/Program.cs
+++ TestCurrencyManager/Program.cs
@@ -16,23 +16,37 @@ namespace TestCurrencyManager
             var dbFile = @"C:\Temp\test.db";
             var connString = string.Format(@"Data Source={0}; Pooling=false; FailIfMissing=false;", dbFile);
 
-            var currencyLoookup = CreateCurrencyLookup();
-                        
-            if (!File.Exists(dbFile))
+            var currencyLookup = CreateCurrencyLookup();
+
+            string input = GetUserInput(currencyLookup);
+
+            if (input != "0")
             {
-                SQLiteConnection.CreateFile(dbFile);
-                InitializeDatabase(connString);
-                InsertCurrency(connString, currencyLoookup);
+                if (!File.Exists(dbFile))
+                {
+                    SQLiteConnection.CreateFile(dbFile);
+                    InitializeDatabase(connString);
+                    InsertBaseCurrency(connString, currencyLookup, input);
+                    InsertCurrency(connString, currencyLookup, input);
+                }
+                else
+                {
+                    if (input != GetCurrentBaseCurrency(connString))
+                    {
+                        Console.WriteLine("Base currency changed. Loading fresh values..");
+                        File.Delete(dbFile);
+                        SQLiteConnection.CreateFile(dbFile);
+                        InitializeDatabase(connString);
+                        InsertBaseCurrency(connString, currencyLookup, input);
+                        InsertCurrency(connString, currencyLookup, input);
+                    }
+                }
+
+                DisplayCurrenyRate(connString, input);
             }
 
-            DisplayCurrenyRate(connString);
-
-            //Console.WriteLine("Deleting DB file now..");
-            //if (File.Exists(dbFile))
-            //{
-            //    File.Delete(dbFile);
-            //}
-
+            Console.WriteLine();
+            Console.WriteLine("Thank you for using out service.");
             Console.ReadLine();
         }
 
@@ -42,7 +56,7 @@ namespace TestCurrencyManager
         /// <param name="connString">SQL Connection String</param>
         private static void InitializeDatabase(string connString)
         {
-            Console.WriteLine("Setting up database..");
+            //Console.WriteLine("Setting up database..");
             using (var factory = new SQLiteFactory())
             {
                 using (System.Data.Common.DbConnection dbConn = factory.CreateConnection())
@@ -67,7 +81,7 @@ namespace TestCurrencyManager
                     if (dbConn.State != System.Data.ConnectionState.Closed) dbConn.Close();
                 }
             }
-            Console.WriteLine("Setup finished..");
+            //Console.WriteLine("Setup finished..");
             Console.WriteLine();
         }
 
@@ -76,11 +90,11 @@ namespace TestCurrencyManager
         /// </summary>
         /// <param name="connString">SQL Connection String</param>
         /// <param name="paraDict">Lookup dictionary containing currency code and description</param>
-        private static void InsertCurrency(string connString, Dictionary<string, string> paraDict)
+        private static void InsertCurrency(string connString, Dictionary<string, string> paraDict, string paraBaseCurrency)
         {
             //Console.WriteLine("Begin InsertCurrency");
 
-            var url = "https://api.fixer.io/latest?base=AUD";
+            var url = "https://api.fixer.io/latest?base=" + paraBaseCurrency;
             var currencyRates = FetchJson._download_serialized_json_data<Currency>(url);
 
             using (var factory = new SQLiteFactory())
@@ -128,13 +142,56 @@ namespace TestCurrencyManager
         }
 
         /// <summary>
+        /// Inserts the base currency with exchange rate = 1
+        /// </summary>
+        /// <param name="connString">SQL Connection String</param>
+        /// <param name="paraDict">Lookup dictionary containing currency code and description</param>
+        private static void InsertBaseCurrency(string connString, Dictionary<string, string> paraDict, string paraBaseCurrency)
+        {
+            using (var factory = new SQLiteFactory())
+            {
+                using (System.Data.Common.DbConnection dbConn = factory.CreateConnection())
+                {
+                    dbConn.ConnectionString = connString;
+                    dbConn.Open();
+                    using (System.Data.Common.DbCommand cmd = dbConn.CreateCommand())
+                    {
+                        string description = string.Empty;
+                        if (paraDict.ContainsKey(paraBaseCurrency))
+                        {
+                            description = paraDict[paraBaseCurrency];
+
+                            //parameterized insert
+                            cmd.CommandText = @"INSERT INTO Currencies ([Code],[Description],[ExchangeRate]) VALUES(@code,@description,'1.00');";
+
+                            var p1 = cmd.CreateParameter();
+                            p1.ParameterName = "@code";
+                            p1.Value = paraBaseCurrency;
+
+                            var p2 = cmd.CreateParameter();
+                            p2.ParameterName = "@description";
+                            p2.Value = description;
+
+                            cmd.Parameters.Add(p1);
+                            cmd.Parameters.Add(p2);
+
+                            cmd.ExecuteNonQuery();
+                        }
+                    }
+                    if (dbConn.State != System.Data.ConnectionState.Closed) dbConn.Close();
+                }
+            }
+            //Console.WriteLine("End InsertCurrency");
+        }
+
+        /// <summary>
         /// Displays the currency rates stored in the local database
         /// </summary>
         /// <param name="connString"></param>
-        private static void DisplayCurrenyRate(string connString)
+        private static void DisplayCurrenyRate(string connString, string paraBaseCurrency)
         {
             Console.WriteLine("--------");
-            Console.WriteLine("1 AUD is ");
+            Console.WriteLine("1 {0} is ", paraBaseCurrency);
             Console.WriteLine("--------");
             using (var factory = new SQLiteFactory())
             {
@@ -154,6 +211,7 @@ namespace TestCurrencyManager
                                 string description = reader.GetString(1);
                                 string currency = description + "(" + code + ")";
                                 decimal rate = reader.GetDecimal(2);
+                                if(code == paraBaseCurrency) { continue; }
                                 Console.WriteLine("       = {0} {1}", rate, currency);
                             }
                         }
@@ -162,17 +220,17 @@ namespace TestCurrencyManager
                     if (dbConn.State != System.Data.ConnectionState.Closed) dbConn.Close();
                 }
             }
-            Console.WriteLine("*******************");
+            Console.WriteLine("**************************************");
         }
 
         /// <summary>
         /// Updates the local database file with latest currency rates
         /// </summary>
         /// <param name="connString"></param>
-        private static void UpdateCurrencyRate(string connString)
+        private static void UpdateCurrencyRate(string connString, string paraBaseCurrency)
         {
             Console.WriteLine("Updating currency rates...");
-            var url = "https://api.fixer.io/latest?base=AUD";
+            var url = "https://api.fixer.io/latest?base=" + paraBaseCurrency;
             var currencyRates = FetchJson._download_serialized_json_data<Currency>(url);
 
             using (var factory = new SQLiteFactory())
@@ -210,6 +268,41 @@ namespace TestCurrencyManager
         }
 
         /// <summary>
+        /// Gets the current base currency in the system
+        /// </summary>
+        /// <param name="connString"></param>
+        /// <returns></returns>
+        private static string GetCurrentBaseCurrency(string connString)
+        {
+            string result = string.Empty;
+
+            using (var factory = new SQLiteFactory())
+            {
+                using (System.Data.Common.DbConnection dbConn = factory.CreateConnection())
+                {
+                    dbConn.ConnectionString = connString;
+                    dbConn.Open();
+                    using (System.Data.Common.DbCommand cmd = dbConn.CreateCommand())
+                    {
+                        //read from the table
+                        cmd.CommandText = @"SELECT [Code] FROM Currencies WHERE [ExchangeRate] = 1.00;";
+                        using (System.Data.Common.DbDataReader reader = cmd.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                result = reader.GetString(0);
+                            }
+                        }
+                        cmd.Dispose();
+                    }
+                    if (dbConn.State != System.Data.ConnectionState.Closed) dbConn.Close();
+                }
+            }
+
+            return result;
+        }
+
+        /// <summary>
         /// Returns a dictionary containing currency code and their description
         /// </summary>
         /// <returns></returns>
@@ -232,6 +325,48 @@ namespace TestCurrencyManager
             temp.Add("AUD", "Australian Dollar");
 
             return temp;
+        }
+
+        /// <summary>
+        /// Asks user for his/her choice of base currency
+        /// </summary>
+        /// <param name="paraDict">Lookup dictionary containing currency code and description</param>
+        /// <returns>Base currency code</returns>
+        private static string GetUserInput(Dictionary<string, string> paraDict)
+        {
+            string userInput = string.Empty;
+            int count = 1;
+            bool result = true;
+
+            do
+            {
+                Console.WriteLine("Please select a base currency from the following list :");
+                Console.WriteLine("-------------------------------------------------------");
+
+                foreach(KeyValuePair<string, string> entry in paraDict)
+                {
+                    Console.WriteLine("{0}. {2} ({1})", count, entry.Key, entry.Value);
+                    count++;
+                }
+
+                Console.WriteLine("Press any other character to exit.");
+                Console.WriteLine("Enter your option (E.g. AUD)");
+                userInput = Console.ReadLine();
+                userInput = userInput.ToUpper();
+
+                if (paraDict.ContainsKey(userInput))
+                {
+                    result = false;
+                }
+                else
+                {
+                    userInput = "0";
+                    result = false;
+                }
+
+            } while (result);
+
+            return userInput;
         }
     }
 }
