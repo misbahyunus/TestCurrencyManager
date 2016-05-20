2ee68f50815dda2f5ddd9bd66ac5c13900ae3de5
Misbah Yunus <misbah.yunus@outlook.com>
2016-05-20 16:45:30 +1000

    Added a new project which has a WPF UI to perform the same action.

---
+++ TestCurrencyManager.Wpf/Model/Currency.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace TestCurrencyManager.Wpf.Model
+{
+    public class Currency
+    {
+        public string _base { get; set; }
+        public string date { get; set; }
+        public Dictionary<string, decimal> rates { get; set; }
+    }
+}
